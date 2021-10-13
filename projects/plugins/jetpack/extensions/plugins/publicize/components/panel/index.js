88066d586c357675e7199e411b40c43ff6cde286
retrofox <rdsuarez@gmail.com>
2021-10-13 18:17:11 -0300

    [not verified] show RePublicize upsell for sites with free plan

--- projects/plugins/jetpack/extensions/plugins/publicize/components/panel/index.js
+++ projects/plugins/jetpack/extensions/plugins/publicize/components/panel/index.js
@@ -7,10 +7,15 @@
  */
 
 /**
+ * External dependencies
+ */
+import classNames from 'classnames';
+
+/**
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { PanelBody, PanelRow, ToggleControl } from '@wordpress/components';
+import { PanelBody, PanelRow, ToggleControl, Button } from '@wordpress/components';
 import { useState } from '@wordpress/element';
 
 /**
@@ -22,6 +27,8 @@ import PublicizeTwitterOptions from '../twitter/options';
 import useSelectSocialMediaConnections from '../../hooks/use-social-media-connections';
 import { usePostJustPublished } from '../../hooks/use-saving-post';
 import usePublicizeConfig from '../../hooks/use-publicize-config';
+import { getRequiredPlan } from '../../../../shared/plan-utils';
+import useUpgradeFlow from '../../../../shared/use-upgrade-flow';
 
 const PublicizePanel = ( { prePublish } ) => {
 	const { refresh, hasConnections, hasEnabledConnections } = useSelectSocialMediaConnections();
@@ -33,7 +40,7 @@ const PublicizePanel = ( { prePublish } ) => {
 	 * Check whether the Republicize feature is enabled.
 	 * it can be defined via the `jetpack_block_editor_republicize_feature` backend filter.
 	 */
-	const { isRePublicizeFeatureEnabled } = usePublicizeConfig();
+	const { isRePublicizeFeatureEnabled, isRePublicizeFeatureUpgradable } = usePublicizeConfig();
 
 	// Refresh connections when the post is just published.
 	usePostJustPublished(
@@ -47,11 +54,38 @@ const PublicizePanel = ( { prePublish } ) => {
 		[ hasEnabledConnections, refresh ]
 	);
 
+	const requiredPlan = getRequiredPlan( 'republicize' );
+
+	const [ checkoutUrl, goToCheckoutPage, isRedirecting ] = useUpgradeFlow( requiredPlan );
+
 	return (
 		<PanelBody title={ __( 'Share this post', 'jetpack' ) }>
 			<div>
 				{ __( "Connect and select the accounts where you'd like to share your post.", 'jetpack' ) }
 			</div>
+
+			{ isRePublicizeFeatureEnabled && isRePublicizeFeatureUpgradable && (
+				<div className="jetpack-publicize__upsell">
+					<div className="jetpack-publicize__upsell-description">
+						{ __(
+							'To re-publicize and schedule a post, you need to upgrade to the Personal Plan',
+							'jetpack'
+						) }
+					</div>
+
+					<Button
+						href={ isRedirecting ? null : checkoutUrl } // Only for server-side rendering, since onClick doesn't work there.
+						onClick={ goToCheckoutPage }
+						target="_top"
+						className={ classNames( 'jetpack-publicize__upsell-button is-primary', {
+							'jetpack-upgrade-plan__hidden': ! checkoutUrl,
+						} ) }
+						isBusy={ isRedirecting }
+					>
+						{ isRedirecting ? __( 'Redirecting…', 'jetpack' ) : __( 'Upgrade now', 'jetpack' ) }
+					</Button>
+				</div>
+			) }
 
 			{ isRePublicizeFeatureEnabled && (
 				<PanelRow>
